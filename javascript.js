1b6bd0f826044aa4659326f28b86a21613a062bb
Elijah <eabrown0220@gmail.com>
2025-01-05 22:02:41 -0500

    Add getHumanChoice function to javascript.js

--- javascript.js
+++ javascript.js
@@ -10,3 +10,26 @@ function getComputerChoice(){
         return "Scissors";
     }
 }
+
+function getHumanChoice(){
+    let choice = prompt("Rock, Paper, or Scissors");
+    choice = choice.toLowerCase();
+    if(choice === "rock"){
+        console.log("Human Rock");
+        return("Rock");
+    }
+    else if(choice === "paper"){
+        console.log("Human Paper");
+        return("Paper");
+    }
+    else if(choice === "scissors"){
+        console.log("Human Scissors");
+        return("Scissors")
+    }
+    else{
+        console.log("Invalid");
+        getHumanChoice();
+    }
+}
+
+getHumanChoice();
